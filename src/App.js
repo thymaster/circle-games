e332ae6433204a5cbfefb2addb93511512f73b66
Anish Sarkar <36694496+thymaster@users.noreply.github.com>
2025-12-19 01:04:13 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import { useRef, useState } from "react";
+import "./styles.css";
+
+const COLORS = ["red", "blue", "green", "orange", "purple"];
+const DIAMETER = 50;
+const getRandomColor = (min, max) => {
+  min = Math.floor(min);
+  max = Math.ceil(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const Circle = ({ clientX, clientY, color }) => {
+  const radius = DIAMETER / 2;
+  return (
+    <span
+      style={{
+        width: `${DIAMETER}px`,
+        height: `${DIAMETER}px`,
+        borderRadius: "50%",
+        position: "absolute",
+        top: clientY - radius,
+        left: clientX - radius,
+        backgroundColor: `${color}`,
+      }}
+    ></span>
+  );
+};
+
+export default function App() {
+  const [circles, setCircles] = useState([]);
+  const [stacks, setStacks] = useState([]);
+  const canvasRef = useRef();
+
+  const generateCircle = (event) => {
+    if (canvasRef.current) {
+      const { clientX, clientY } = event;
+      const rect = canvasRef.current.getBoundingClientRect();
+      const _clientX = clientX - rect.left;
+      const _clientY = clientY - rect.top;
+      const colorIndex = getRandomColor(1, COLORS.length);
+      const color = COLORS[colorIndex - 1];
+
+      setCircles((prev) => {
+        return [
+          ...prev,
+          { id: Date.now(), clientX: _clientX, clientY: _clientY, color },
+        ];
+      });
+    }
+  };
+  const undo = () => {
+    const lastCircle = circles.pop();
+    setStacks((prev) => [...prev, lastCircle]);
+    setCircles([...circles]);
+  };
+  const redo = () => {
+    const lastCircle = stacks.pop();
+    setCircles((prev) => [...prev, lastCircle]);
+    setStacks([...stacks]);
+  };
+  const reset = () => {
+    setCircles([]);
+    setStacks([]);
+  };
+
+  const isResetDisabled = circles.length === 0 && stacks.length === 0;
+  const isUndoDisabled = circles.length === 0;
+  const isRedoDisabled = stacks.length === 0;
+
+  return (
+    <div className="App">
+      <div className="container" onClick={generateCircle} ref={canvasRef}>
+        {circles.map((ele) => (
+          <Circle key={ele.id} {...ele} />
+        ))}
+      </div>
+      <div className="action-area">
+        <button disabled={isUndoDisabled} onClick={undo}>
+          Undo
+        </button>
+        <button disabled={isRedoDisabled} onClick={redo}>
+          Redo
+        </button>
+        <button disabled={isResetDisabled} onClick={reset}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+}
